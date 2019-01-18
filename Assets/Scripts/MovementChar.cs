1039c87d20f5190f6b986327c8ff6edb446ca4a2
Rafael Clementino Araujo Soares <rafael.soares@gmail.com>
2019-01-18 17:13:31 -0200

    Update MovementChar.cs

--- Assets/Scripts/MovementChar.cs
+++ Assets/Scripts/MovementChar.cs
@@ -6,17 +6,23 @@ using UnityEngine;
 public class MovementCha : MonoBehaviour {
   
   private Rigidbody myRigidbody;
+  privater Quaternion rotation;
   
   void Awake(){
     myRigidbody = GetComponent<Rigidbody>();
   }
+  		
   
-  
-  void Movement(Vector3 direction , float velocity)
+  public void Movement(Vector3 direction , float velocity)
   {
     myRigidbody.MovePosition(
 				myRigidbody.position + 
 				(direction.normalized * velocity * Time.deltaTime)) ;
+  }
+	
+  public void Rotation(Vector3 direction ){
+	 rotation =Quaternion.LookRotation(direction);
+	 myRigidbody.MoveRotation(rotation); 
   }
 
 }
