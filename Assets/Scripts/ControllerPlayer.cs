cf3be1b881b760a8b69a51f5b33bc0fd32166aa2
Rafael Clementino Araujo Soares <rafael.soares@gmail.com>
2019-01-18 12:41:26 -0200

    Update ControllerPlayer.cs

--- Assets/Scripts/ControllerPlayer.cs
+++ Assets/Scripts/ControllerPlayer.cs
@@ -19,6 +19,7 @@ public class ControllerPlayer : MonoBehaviour {
 	private Animator animatorPlayer;
 	public int lifePlayer = 100;
 	public ControllerInterface scriptControllerInterface;
+	public AudioClip SongDamage;
 	
 	/// Start is called on the frame when a script is enabled just before
 	/// any of the Update methods is called the first time.
