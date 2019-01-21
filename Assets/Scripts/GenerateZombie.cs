52c32f55c2a460001866a6ccdcbaac4796ae2b2c
Rafael Clementino Araujo Soares <rafael.soares@gmail.com>
2019-01-21 15:28:59 -0200

    Update GenerateZombie.cs

--- Assets/Scripts/GenerateZombie.cs
+++ Assets/Scripts/GenerateZombie.cs
@@ -7,6 +7,7 @@ public class GenerateZombie : MonoBehaviour{
     public GameObject zombie;
     private  float countTime = 0;
     public float timeSpawnZombie = 1;
+    public LayerMask zumbiLayer;
 
     // Start is called before the first frame update
     void Start(){
@@ -16,9 +17,28 @@ public class GenerateZombie : MonoBehaviour{
     // Update is called once per frame
     void Update(){
         countTime += Time.deltaTime;
-        if (countTime >= timeSpawnZombie){
-            Instantiate(zombie,transform.position, transform.rotation);
+        if (countcTime >= timeSpawnZombie){
+            StartCoroutine(GenerateZombie());
             countTime = 0;        
         }
     }
+    
+    IEnumerator RandomizePosition(){
+        Vector3 posiotnCreate = RandonPosition();
+        Collider[] colision = Physics.OverlapSphere(posiotnCreate,1,zumbiLayer);
+        
+        while (colision.Length > 0){
+            posiotnCreate = RandonPosition();
+            colision = Physics.OverlapSphere(posiotnCreate,1,zumbiLayer);
+            yield return null;
+        }
+    }
+      
+	Vector3 RandonPosition(){
+		Vector3 position = Randon.insideUnitShpere * 3;
+		position += transform.position;
+		position.y = transform.position.y;
+		return position;
+	}
+    
 }
