2364bdee8dd08a955feda6eca4e0bb7d01ca5596
Rafael Clementino Araujo Soares <rafael.soares@gmail.com>
2019-01-21 11:47:20 -0200

    Update AnimationChar.cs

--- Assets/Scripts/AnimationChar.cs
+++ Assets/Scripts/AnimationChar.cs
@@ -11,6 +11,15 @@ public class AnimationChar : MonoBehaviour {
   }
   
   public void Attack(bool isAttack){
+    
+	//	if (direction != Vector3.zero){
+//			animatorPlayer.SetBool("isWalking",true);
+	//		animatorPlayer.SetBool("isIdle",false);
+		//}else{
+	//		animatorPlayer.SetBool("isWalking",false);
+	//		animatorPlayer.SetBool("isIdle",true);
+//		}
+    
     myAnimator.SetBool("isAttack",isAttack);
   }
 
